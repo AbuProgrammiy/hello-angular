5451634592ab47af7393d6cf0bfcd9879fb2b6eb
Abdukholiq G'ijdvoniy <abdukholiq23@gmail.com>
2026-01-31 23:05:53 +0500

    feat(header): create a brand new liquid-glass navbar

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { RouterLink } from "@angular/router";
+import { SelectModule } from 'primeng/select';
 
 @Component({
   selector: 'app-header',
   imports: [
-    ButtonModule
-  ],
+    ButtonModule,
+    RouterLink,
+    SelectModule
+],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
