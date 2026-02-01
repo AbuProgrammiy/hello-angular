8a3032242f37eea3d22d33f573309435493e5ace
Abdukholiq G'ijdvoniy <abdukholiq23@gmail.com>
2026-02-01 06:51:49 +0500

    feat(car-card): create card for cars

--- src/app/features/show-room/show-room.ts
+++ src/app/features/show-room/show-room.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { Button } from "primeng/button";
+import { CarCard } from "../../shared/components/car-card/car-card";
 
 @Component({
   selector: 'app-show-room',
-  imports: [],
+  imports: [
+    InputTextModule,
+    CardModule,
+    Button,
+    CarCard
+],
   templateUrl: './show-room.html',
   styleUrl: './show-room.scss',
 })
